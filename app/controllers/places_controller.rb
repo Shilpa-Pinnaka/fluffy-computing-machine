ffec5f0e2ce4537f1b17dc8433b00cca203bdfa1
Shilpa-Pinnaka <silpa.isha@gmail.com>
2018-02-07 18:42:09 +0000

    We made changes to places_controller.rb and show.html.erb.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -31,9 +31,14 @@ def update
   @place = Place.find(params[:id])
   @place.update_attributes(place_params)
    redirect_to root_path
-   
+
 end
 
+def destroy
+  @place = Place.find(params[:id])
+  @place.destroy
+  redirect_to root_path
+end
 
 private
 
