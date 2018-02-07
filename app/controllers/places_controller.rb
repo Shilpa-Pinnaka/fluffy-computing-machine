cd19b87e7f49da7296f710136212b38d7438b1b9
Shilpa-Pinnaka <silpa.isha@gmail.com>
2018-02-07 00:28:58 +0000

    We made changes to places_controller.rb

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,4 +9,17 @@ def new
   @place = Place.new
   
 end
+
+def create
+  @place = Place.create(place_params)
+  redirect_to root_path
+
+end
+
+private
+
+def place_params
+  params.require(:place).permit(:name, :address, :description)
+end
+
 end
