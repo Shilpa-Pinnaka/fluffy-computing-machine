05848dc4f5f0dd3dec9c65adde9493e2fe072c27
Shilpa-Pinnaka <silpa.isha@gmail.com>
2018-02-07 13:52:38 +0000

    We made changes to places_controller.rb,index.html.erb,place.rb,user.rb, we did a migration.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
 def index
   @places = Place.all
@@ -11,7 +12,7 @@ def new
 end
 
 def create
-  @place = Place.create(place_params)
+  @place =  current_user.places.create(place_params)
   redirect_to root_path
 
 end
