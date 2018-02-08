072abd0f937a48d04efaf2294ae59cd35b62047c
Shilpa-Pinnaka <silpa.isha@gmail.com>
2018-02-08 15:56:25 +0000

    We made changes to master.scss and places_controller.rb

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,7 +14,14 @@ end
 
 def create
   @place =  current_user.places.create(place_params)
+  if @place.valid?
+
   redirect_to root_path
+
+else
+  render :new, status: :unprocesssable_entity
+
+end
 
 end
 
@@ -40,7 +47,13 @@ def update
   end
 
   @place.update_attributes(place_params)
+  if @place.valid?
    redirect_to root_path
+
+ else
+  render :edit, status: :unprocesssable_entity
+
+end
 
 end
 
